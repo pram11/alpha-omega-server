8da09491b24b0d91da84c6838fd69249eb3b1855
pram11 <an6207@gmail.com>
2019-11-27 11:54:20 +0900

    json 파일 다루기 추가

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ from flask import Flask, jsonify,request
 import json
 app = Flask(__name__)
 
-with open('./status.json','w',encoding='utf-8') as json_file:
-	json_data = json.loads(json_file)
+with open('./status.json',encoding='utf-8') as json_file:
+	json_data = json.load(json_file)
 @app.route('/status',methods=["POST","GET"])
 def status():
 	if request.method=="POST":
-		print("test")
-		print(request.get_json())
+		print(request.get_json(force=True))
 		json_request = request.get_json()
-		print(json_request['status'])
-		return jsonify(json_data)
+		json_data['status'] = json_request['status']
+		with open('./status.json','w',encoding='utf-8')as js_w:
+			json.dump(json_data,js_w)
+
+		print(json_request)
+		return jsonify(sataus=json_data['status'])
 	elif request.method=="GET":
 		return jsonify(json_data) 
 
@@ -19,9 +22,15 @@ def status():
 @app.route('/token',methods=['POST'])
 def getToken():
 	if request.method=="POST":
+		json_request = request.get_json()
+		json_data['token']=json_request['token']
+		with open('./status.json','w',encoding='utf-8')as js_w:
+			json.dump(json_data,js_w)
 		print("getToken")
 		print(request.get_json())
-		return jsonify(json_data)
+		json_request = request.get_json()
+		print(json_request['status'])
+		return jsonify(json_data['status'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8995,debug=True)
+	app.run(host='0.0.0.0',port=8995,debug=True)
