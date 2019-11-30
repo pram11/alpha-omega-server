d433cda1ca02d0ad6911701efdcead1b7ad20826
pram11 <an6207@gmail.com>
2019-11-30 14:39:45 +0900

    리눅스 서버 상에서 개발용 이전

--- arduino.py
+++ arduino.py
@@ -4,18 +4,22 @@ import json
 
 
 ser = serial.Serial(
-    port="COM7",
+    port="COM3",
     baudrate=9600
 )
 while True:
-    print("status checking...")
+    print("status checking start")
     with open('./status.json', encoding='utf-8') as json_file:
         json_data = json.load(json_file)
     print(json_data['status'])
     if json_data['status']=='fire':
-        ser.write('f'.encode())
+        ser.write('status:fire\n'.encode())
         print(ser.readline())
     elif json_data['status']=='normal':
-        ser.write('n'.encode())
+        ser.write('status:normal\n'.encode())
         print(ser.readline())
+    val=''
+    val = ser.readline()
+    print(val)
+    print("turn end!")
     time.sleep(2)
